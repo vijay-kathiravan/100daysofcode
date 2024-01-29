faeab740930fb526e138e6644deb6269457c2487
vijay <17bmc037@mcet.in>
2024-01-29 17:58:33 -0500

    100 days of python day2 commit

---
+++ Beginner/Day2.0.py
@@ -0,0 +1,5 @@
+# Adding the digits of the number using data types and other methods.
+number2add = input("Give a number to add!\t")
+number2addstr = str(number2add)
+addition = int(number2add[0]) + int(number2add[1])
+print(addition)
