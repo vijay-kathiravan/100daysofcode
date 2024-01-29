e8d4c98e5a9efe4afad1e98be3411ec6be4db4d5
vijay <17bmc037@mcet.in>
2024-01-28 22:53:59 -0500

    100 days of python day1 commit

--- Beginner/Day1.py
+++ Beginner/Day1.py
@@ -1,7 +1,7 @@
 # Vijay
 var = input('Give a word to count the number of strings in it!\n')
-flag = 0
+red1 = 0
 for i in var:
     if i in var:
-        flag+=1
-print(flag)
+        red1+=1
+print(red1)
