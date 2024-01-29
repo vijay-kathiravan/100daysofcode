c9b25ef91f177e0cf5239a610e91adbbddef20e9
vijay <17bmc037@mcet.in>
2024-01-28 23:30:18 -0500

    100 days of python day1 commit

---
+++ Projects/Day1_NameBandGenerator.py
@@ -0,0 +1,6 @@
+Greet = input('Hello, Myself Python Code! Welcome to Band Name generator, how should i be addressing you?\n')
+print("Hello " + Greet + " Greetings of the day!")
+city = input("May i know which city did you grew up on?\n")
+pet = input("May i know the name of your pet you had/have/will have ?\n")
+print("Your band name could be " + city + pet)
+
