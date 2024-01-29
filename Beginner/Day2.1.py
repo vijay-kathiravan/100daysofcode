71a0e253caa87804752cb3e0901dff9f759b93ab
vijay <17bmc037@mcet.in>
2024-01-29 18:34:57 -0500

    100 days of python day2 commit

---
+++ Beginner/Day2.1.py
@@ -0,0 +1,3 @@
+age = int(input("Kindly provide your current age to see how long can you possibly live on earth in weeks\t"))
+projected_age = round((90 - age) * 52.1)
+print(f'You are projected to have {projected_age} weeks left in your lifetime! Live the way you love, YOLO!')
