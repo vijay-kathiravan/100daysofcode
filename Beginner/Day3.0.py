19871eb6f70c5837d3d898214d654a7c4ce04121
vijay <17bmc037@mcet.in>
2024-01-30 22:25:15 -0500

    100 days of python day3 commit

---
+++ Beginner/Day3.0.py
@@ -0,0 +1,5 @@
+number = int(input("Give a number to check if it is odd or even!\t"))
+if number%2==0:
+    print("The number {} is even".format(number))
+else:
+    print("The number {} is odd".format(number))
