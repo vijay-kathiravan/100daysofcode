d6ba57a2dea7d65cd106b42a4c39f046675c6ffb
vijay <17bmc037@mcet.in>
2024-02-25 00:28:01 -0500

    100 days of python day4 commit
    Signed-off-by: vijay <17bmc037@mcet.in>

--- Beginner/Day4.3.py
+++ Beginner/Day4.3.py
@@ -4,14 +4,8 @@ line3 = ['[D]','[D]','[D]']
 map = [line1,line2,line3]
 position = input("Give a position to hide the Treasure\t")
 row,column = position[1],position[0]
-#print(row,column)
-if column == 'A':
-    column= 0
-elif column == 'B':
-    column= 1
-else:
-    column = 2
-#print(column)
+alphlist = ['A','B','C']
+column = alphlist.index(column)
 map[int(row)][column] = 'X'
 print("Now the treasure is hidden!")
 x = input("Do you want to see the treasure?\t")
